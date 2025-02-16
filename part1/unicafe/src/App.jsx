434518cb5c02e45845474c5cd22132f7d5e9aeae
TayunCodes <tayunparkdev@gmail.com>
2025-02-16 16:11:06 -0500

    1.7 unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,39 +1,46 @@
 import { useState } from 'react'
+const average = []
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const average = all === 0 ? 0 : (good-bad)/all //first part is to ensure we don't divide by (all=)0
+  const positive = `${all === 0 ? 0 : (good)/all*100} %`
 
   const increaseGood = () => {
-
-    console.log('increasing good, value before', good)
+    // console.log('increasing good, value before', good)
     setGood(good + 1)
+    setAll(all + 1)
   }
 
   const increaseNeutral = () => {
-
-    console.log('increasing neutral, value before', neutral)
+    // console.log('increasing neutral, value before', neutral)
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
 
   const increaseBad = () => {
-
-    console.log('increasing bad, value before', bad)
+    // console.log('increasing bad, value before', bad)
     setBad(bad + 1)
+    setAll(all + 1)
   }
 
   return (
     <div>
       <Header />
-      <Button onClick={increaseGood} text="good" />
-      <Button onClick={increaseNeutral} text="neutral" />
-      <Button onClick={increaseBad} text="bad" />
+      <Button onClick={increaseGood} text="good" average={average}/>
+      <Button onClick={increaseNeutral} text="neutral" average={average} />
+      <Button onClick={increaseBad} text="bad" average={average} />
       <Statistics />
       <Display type="good" counter={good} />
       <Display type="neutral" counter={neutral} />
       <Display type="bad" counter={bad} />
+      <Display type="all" counter={all} />
+      <Display type="average" counter={average} />
+      <Display type="positive" counter={positive} />
     </div>
   )
 }
