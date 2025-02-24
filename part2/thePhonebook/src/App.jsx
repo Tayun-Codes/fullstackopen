02fa5842d0b9d96a60c04ccff8b5d83e2a360233
TayunCodes <tayunparkdev@gmail.com>
2025-02-23 22:15:49 -0500

    2.6 thePhonebook

---
+++ part2/thePhonebook/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  //controlling the form input element
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      content: newName,
+      id: String(persons.length + 1)
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}
+            />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((name) => (
+          <Person key={name.id} name={name} />
+        ))}
+    </div>
+  )
+}
+
+export default App
---
+++ part2/thePhonebook/src/components/Person.jsx
@@ -0,0 +1,5 @@
+const Person = ({ name }) => {
+    return <li>{name.content}</li>
+}
+
+export default Person
